c6f7467c4bd8d01cf3a27e4b5561a7b970947354
Ryan <ryan@persist-ai.com>
2025-06-30 17:45:24 -0700

    made progress, converted my docstore to a sqlite db instead of inmemorystore, fixed folder organization

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # project report-generator
-persist ai final project report generator for kenan and the lab team
+project report generator for kenan and the lab team
--- onenote.py
+++ onenote.py
@@ -23,13 +23,13 @@ site_id = "persistai.sharepoint.com,44d5d7d9-9894-437b-a938-37f26f5ad066,95dec0f
 notebook_url = f"https://graph.microsoft.com/v1.0/sites/{site_id}/onenote/notebooks"
 notebook_response = requests.get(notebook_url, headers=headers)
 notebook_data = notebook_response.json()
-#print(json.dumps(notebook_data, indent=2))
+print(json.dumps(notebook_data, indent=2))
 
 #GET THE SECTIONS
 sections_url = f"https://graph.microsoft.com/v1.0/sites/persistai.sharepoint.com,44d5d7d9-9894-437b-a938-37f26f5ad066,95dec0fb-afca-4a24-af80-faf5e2f9cc75/onenote/notebooks/1-091c59f7-0e8d-4ac4-9bb6-34e621412f2f/sections"
 section_response = requests.get(sections_url, headers=headers)
 section_data = section_response.json()
 section_id = section_data.get('value', 'id')
-print(section_id)
 
 #GET THE PAGES AND THEIR CONTENT IN EACH SECTION
+
